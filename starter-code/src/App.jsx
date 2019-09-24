94b010fcb2ee449763c48aa786505d4ef492767f
Brandy McArthur <bdm.mcarthur@gmail.com>
2019-09-24 11:16:26 +0100

    Iteration 2

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -4,11 +4,30 @@ import contacts from "./contacts.json";
 import "./App.css";
 
 const contactsArray = contacts.slice(0, 4);
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: contactsArray
+    };
+    this.add = this.add.bind(this);
+  }
+
+  add() {
+    this.setState({
+      person: {
+        ...this.state.person,
+        person: contactsArray.push(
+          contacts[Math.floor(Math.random() * Math.floor(contacts.length))]
+        )
+      }
+    });
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.add}>Add Random Person</button>
         <table>
           <tr>
             <th>Picture</th>
